b1403a66940c5b7e31bfe16b879ed3d0de507e7b
kagemeka <kagemeka1@gmail.com>
2021-06-04 22:52:36 +0900

    transfer for v0.2.0

---
+++ archive/kagemeka/ds/__init__.py
@@ -0,0 +1,23 @@
+from .audio import (
+  AudioProc,
+)
+
+
+from .img import (
+  VideoProps,
+  Video,
+  VideoProc,
+  ImgProc,
+  ChangeProps,
+  FrameSz,
+  ColorGenerator,
+)
+
+
+from .nlp import (
+  JPTokenizer,
+  DicCfg,
+  JPTagger,
+  TxtProc,
+  WikiCorpusGen,
+)
