b880d2f345ed04bccb4c229049f670f7fa828135
kagemeka <kagemeka1@gmail.com>
2021-07-15 07:49:00 +0900

    enhance: add twitter package (#45)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-version = '0.1.8'
+version = '0.1.10'
 
 import typing
 import pathlib
---
+++ src/kgmk/twitter/users/__init__.py
@@ -0,0 +1,8 @@
+from .expansions import (
+  Expansions,
+)
+
+from .convert_user import (
+  ConvertUser,
+)
+
