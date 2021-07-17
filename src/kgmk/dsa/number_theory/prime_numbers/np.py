aedd9f8e9b51d3cd6cc7480248e62996fbb14caa
kagemeka <kagemeka1@gmail.com>
2021-07-18 07:33:15 +0900

    enhance: update dsa, number_theory

---
+++ src/kgmk/dsa/number_theory/prime_numbers/np.py
@@ -0,0 +1,40 @@
+from \
+  ..sieve_of_eratosthenes.np \
+import (
+  SieveOfEratosthenes,
+)
+# TODO cut below
+
+
+import numpy as np
+import typing
+
+
+
+class PrimeNumbers():
+  def __getitem__(
+    self,
+    i: int,
+  ) -> int:
+    return self.__a[i] 
+
+
+  def __init__(
+    self, 
+    n: int = 1 << 20,
+  ) -> typing.NoReturn:
+    fn = SieveOfEratosthenes()
+    a = np.flatnonzero(fn(n))
+    self.__a = a
+
+
+  def __iter__(
+    self,
+  ) -> typing.Iterator[int]:
+    return iter(self.__a)
+
+
+  def __repr__(
+    self,
+  ) -> str:
+    return f'{self.__a}'
