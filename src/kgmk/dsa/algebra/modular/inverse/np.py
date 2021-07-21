bc6eb960fcb5544bb56f2096c63b355c59371ee5
kagemeka <kagemeka1@gmail.com>
2021-07-21 10:37:46 +0900

    enhance: implement dsa, numpy modular inverse (#81)

---
+++ src/kgmk/dsa/algebra/modular/inverse/np.py
@@ -0,0 +1,40 @@
+from \
+  kgmk.dsa.algebra.modular \
+  .factorial.np \
+import (
+  ModFactorial,
+)
+
+
+# TODO cut below 
+
+
+import typing
+
+
+
+class Inverse():
+  def __call__(
+    self,
+    i: int,
+  ) -> int:
+    return self[i]
+
+
+  def __getitem__(
+    self,
+    i: int,
+  ) -> int:
+    return self.__a[i]
+
+
+  def __init__(
+    self,
+    n: int,
+    modulo: int,
+  ) -> typing.NoReturn:
+    m = modulo
+    fn = ModFactorial(m)
+    a = fn.inv(n)
+    a[1:] *= fn(n - 1)
+    self.__a = a % m
